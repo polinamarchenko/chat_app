ec663447fa982f8ca209b360ae91d8a47f8344ba
Polina Marchenko <polina.marchenko@yahoo.com>
2017-06-23 21:47:15 -0700

    Add scrollToBottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,23 @@
 var socket = io();
+
+function scrollToBottom() {
+  //Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', () => {
   console.log('connected to server');
 })
@@ -13,6 +32,7 @@ socket.on('newMessage', function(message) {
     createAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -23,13 +43,8 @@ socket.on('newLocationMessage', function(message) {
     createAt: formattedTime,
     url: message.url
   });
-
-
-  // var a = $('<a target="_blank">My current location</a>');
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
   $('#messages').append(html);
+  scrollToBottom();
 })
 
 socket.on('disconnect', () => {
